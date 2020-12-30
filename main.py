6fbd9e8d104bf3196da166a4894643cfc3603b78
Amirtay Dinmukhammed <amirtayd@mail.ru>
2020-12-30 22:34:25 +0600

    .env addition

--- main.py
+++ main.py
@@ -9,15 +9,17 @@ from telethon.helpers import TotalList
 load_dotenv()
 
 n = 0
-ss = 1000
-# количество песен в группе
+ss = os.getenv("SONGS_QUANTITY")
+# number of songs in the group
+
+channel_name = os.getenv("CHANNEL_NAME")
 
 current_dir = pathlib.Path.cwd()
 
 directory = str(current_dir) + "\data"
 
 api_id = os.getenv("API_ID")
-# ваш api_id
+# your api_id
 api_hash = os.getenv("API_HASH")
 session = str("anon")
 client = TelegramClient(session, api_id, api_hash)
@@ -26,14 +28,14 @@ client.start()
 
 while True:
 
-    msgs = client.get_messages('kz_music_base', limit=ss)
+    msgs = client.get_messages(channel_name, limit=ss)
     for msg in msgs:
         if msg.media is not None:
             client.download_media(message=msg, file=directory)
 
     time.sleep(3)
     n = n + 1
-    print("Скачано ")
+    print("Downloaded ")
     time.sleep(2)
     if n == 1:
         break
